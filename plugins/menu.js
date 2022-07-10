41a270428e510a83ab678be9efa0fb91b3edb071
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-10 08:59:44 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,22 +4,13 @@ exports.run = {
       client,
       isPrefix
    }) => {
-      let rows = [{
-         title: 'All COMMAND',
-         rowId: `${isPrefix}menutype 1`,
-         description: `𝗠𝙍 𝗬𝗢𝗨𝗖𝗛𝗡 `
-      }
 
-]
-      let text = '𝗠𝙍 𝗬𝗢𝗨𝗖𝗛𝗡.\n\n'
-      text += '◦ *instagram* : http://instagram.com/mr_johan_shezir *(v2.2.0)*\n\n'
-      text += 'Ela konto talib/tilmid, Sift Lya *#bac*'
-      await client.sendList(m.chat, '', text, '', 'click!', [{
-         rows
-      }], m)
-   },
-   error: false
-}
+
+
+
+
+
+
 exports.run = {
    usage: ['menu', 'help', 'bot'],
    async: async (m, {
