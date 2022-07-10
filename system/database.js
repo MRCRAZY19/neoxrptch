b3d027f14d63bb1c60497e38c80cd11a629bff36
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-10 08:15:03 +0100

    Update database.js

--- system/database.js
+++ system/database.js
@@ -89,8 +89,8 @@ module.exports = (m) => {
       global.db.setting = {
          autodownload: true,
          chatbot: true,
-         sk_pack: 'MOHAMED BELLA ❤️',
-         sk_author: '@wox_bella',
+         sk_pack: '𝗠𝙍 𝗬𝗢𝗨𝗖𝗛𝗡',
+         sk_author: '@mr_johan_shezir',
          debug: false,
          error: [],
          pluginDisable: [],
@@ -101,8 +101,8 @@ module.exports = (m) => {
          toxic: ["ajg", "ajig", "anjas", "anjg", "anjim", "anjing", "anjrot", "anying", "asw", "autis", "babi", "bacod", "bacot", "bagong", "bajingan", "bangsad", "bangsat", "bastard", "bego", "bgsd", "biadab", "biadap", "bitch", "bngst", "bodoh", "bokep", "cocote", "coli", "colmek", "comli", "dajjal", "dancok", "dongo", "fuck", "gelay", "goblog", "goblok", "guoblog", "guoblok", "hairul", "henceut", "idiot", "itil", "jamet", "jancok", "jembut", "jingan", "kafir", "kanjut", "kanyut", "keparat", "kntl", "kontol", "lana", "loli", "lont", "lonte", "mancing", "meki", "memek", "ngentod", "ngentot", "ngewe", "ngocok", "ngtd", "njeng", "njing", "njinx", "oppai", "pantek", "pantek", "peler", "pepek", "pilat", "pler", "pornhub", "pucek", "puki", "pukimak", "redhub", "sange", "setan", "silit", "telaso", "tempek", "tete", "titit", "toket", "tolol", "tomlol", "tytyd", "wildan", "xnxx"],
          online: true,
          onlyprefix: '+',
-         owners: ['212632244668'],
-         msg: 'hello my name is Mohamed bella'
+         owners: ['212689349938'],
+         msg: 'hello my name is 𝙼𝚁 𝚈𝙾𝚄𝙲𝙷𝙽'
       }
    }
 }
