daacc61c87b51667d185da49171eee93a2513aea
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-10 23:13:22 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,7 +5,7 @@
       isPrefix
    }) => {
       let rows = [{
-         title: 'ALL COMMAND',
+         title: 'ALL COMMANDS',
          rowId: `${isPrefix}menutype 1`,
          description: `𝗠𝙍 𝗬𝗢𝗨𝗖𝗛𝗡`
       }, {
