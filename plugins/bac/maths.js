8130d0c5ce5b8a309dfd4f84e2da526336a9ee1f
JOHAN-7777 <108243176+MRCRAZY19@users.noreply.github.com>
2022-07-12 20:36:30 +0100

    Update maths.js

--- plugins/bac/maths.js
+++ plugins/bac/maths.js
@@ -15,7 +15,7 @@ exports.run = {
          {
          title: 'LEÇON 2',
          rowId: `${isPrefix}mediafire https://www.mediafire.com/file/4hq1nadce9yq6lz/examens_bac_PC_SC.PHYS_2008_-_2016_avec_corr.rar`,
-         description: `insta : @wox_bella`
+         description: `insta : @mr_johan_shezir`
          }
 
 ]
